3cf7ce97b23f43010e7472dfeccbc5cbef6775b9
mohammedzubairi <mohammed.ab.alzubairi@gmail.com>
2024-05-16 19:03:24 +0300

    completed navigation bar

--- js/app.js
+++ js/app.js
@@ -120,3 +120,20 @@ document.addEventListener("DOMContentLoaded", function () {
       console.error("Error fetching JSON data:", error);
     });
 });
+
+// ---------------- NAVIGATION TOGGLE ---------------------
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle =document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener('click', () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+  } else if (visibility === "true"){
+    primaryNav.setAttribute("data-visible", false);
+  }
+})
+
+// ----------------END of NAVIGATION TOGGLE ---------------------
+

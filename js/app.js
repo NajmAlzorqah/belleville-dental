7e1ae3ac83113aa7da0913af122f83d42b6d2c43
NajmAlzorqah <najmalzorqah@gmail.com>
2024-05-23 04:01:47 +0300

    finshing up the products section [READY] , creating json file, adding the imgs section, and correcting the paths of the imges

--- js/app.js
+++ js/app.js
@@ -49,19 +49,218 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
   }
-
+  
   // Attach the smoothScroll function to navbar links
   var navbarLinks = document.querySelectorAll(".navigation-bar a");
   navbarLinks.forEach(function (link) {
     link.addEventListener("click", smoothScroll);
   });
-
+  
   // Attach the smoothScroll function to footer links
   var footerLinks = document.querySelectorAll(".footer-link");
   footerLinks.forEach(function (link) {
     link.addEventListener("click", smoothScroll);
   });
 });
+//----------------- End of Smooth Scrolling with Offset Adjustment ----------------
+
+// ---------------- Start of Products Filtering and Fetching ---------------------
+/**
+ * Fetch and Display Filtered Items from JSON Data
+ *
+ * This JavaScript code fetches data from a JSON file, creates HTML elements based on the data,
+ * and displays them on the webpage. It also provides filtering functionality to show specific
+ * items based on categories.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  // Fetch JSON data from the server
+  fetch("product.json")
+    .then((response) => response.json()) // Parse the JSON response
+    .then((data) => {
+      // Function to create HTML elements for each item
+      function createItemElement(item) {
+        var itemDiv = document.createElement("div");
+        itemDiv.classList.add("item", item.type);
+        itemDiv.dataset.id = item.id;
+
+        var img = document.createElement("img");
+        img.src = item.image;
+        img.alt = item.alt;
+
+        var infoDiv = document.createElement("div");
+        infoDiv.classList.add("info");
+
+        var contentDiv = document.createElement("div");
+        contentDiv.innerHTML = `
+    <h3>${item.title}</h3>
+    <p>${item.content}</p>
+    `;
+
+        var ratingDiv = document.createElement("div");
+        ratingDiv.classList.add("rating");
+        ratingDiv.innerHTML = getStars(item.rating);
+
+        var ratingValue = document.createElement("p");
+        ratingValue.textContent = `${item.rating}`;
+
+        var rateMessageDiv = document.createElement("div");
+        rateMessageDiv.classList.add("rate-message");
+        rateMessageDiv.textContent = "👈 Give this product a rating ";
+
+        var starsDiv = document.createElement("div");
+        starsDiv.classList.add("stars");
+        starsDiv.dataset.productId = item.id; // Set data attribute to store product ID
+        starsDiv.innerHTML = `
+    <span class="star" data-value="1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px"><path d="M12 .587l3.668 7.568L24 9.748l-6 5.85 1.416 8.269L12 18.896l-7.416 3.97L6 15.598l-6-5.85 8.332-1.593L12 .587z" fill="#F6D43E"/></svg></span>
+    <span class="star" data-value="2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px"><path d="M12 .587l3.668 7.568L24 9.748l-6 5.85 1.416 8.269L12 18.896l-7.416 3.97L6 15.598l-6-5.85 8.332-1.593L12 .587z" fill="#F6D43E"/></svg></span>
+    <span class="star" data-value="3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px"><path d="M12 .587l3.668 7.568L24 9.748l-6 5.85 1.416 8.269L12 18.896l-7.416 3.97L6 15.598l-6-5.85 8.332-1.593L12 .587z" fill="#F6D43E"/></svg></span>
+    <span class="star" data-value="4"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px"><path d="M12 .587l3.668 7.568L24 9.748l-6 5.85 1.416 8.269L12 18.896l-7.416 3.97L6 15.598l-6-5.85 8.332-1.593L12 .587z" fill="#F6D43E"/></svg></span>
+    <span class="star" data-value="5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px"><path d="M12 .587l3.668 7.568L24 9.748l-6 5.85 1.416 8.269L12 18.896l-7.416 3.97L6 15.598l-6-5.85 8.332-1.593L12 .587z" fill="#F6D43E"/></svg></span>
+    `;
+
+        var readDiv = document.createElement("div");
+        readDiv.innerHTML = `<a href="#">Buy Now</a><svg height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 227.096 227.096" xml:space="preserve"><g><g><polygon style="fill:#010002;" points="152.835,39.285 146.933,45.183 211.113,109.373 0,109.373 0,117.723 211.124,117.723 146.933,181.902 152.835,187.811 227.096,113.55"/></g></g></svg>`;
+        readDiv.classList.add("buy-now");
+
+        starsDiv.appendChild(rateMessageDiv);
+        ratingDiv.appendChild(ratingValue);
+        infoDiv.appendChild(contentDiv);
+        infoDiv.appendChild(ratingDiv);
+        infoDiv.appendChild(starsDiv);
+        itemDiv.appendChild(img);
+        itemDiv.appendChild(infoDiv);
+        itemDiv.appendChild(readDiv);
+
+        // Add event listener to each star
+        starsDiv.querySelectorAll(".star").forEach((star) => {
+          star.addEventListener("click", handleRating);
+        });
+
+        return itemDiv;
+      }
+
+      // Function to generate star icons based on rating
+      function getStars(rating) {
+        const starCount = 5;
+        const filledStars = Math.floor(rating);
+        const remainder = rating - filledStars;
+        let starsHTML = "";
+        for (let i = 0; i < filledStars; i++) {
+          starsHTML +=
+            '<span class="star"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px"><path d="M12 .587l3.668 7.568L24 9.748l-6 5.85 1.416 8.269L12 18.896l-7.416 3.97L6 15.598l-6-5.85 8.332-1.593L12 .587z" fill="#F6D43E"/></svg></span>';
+        }
+
+        if (remainder > 0) {
+          starsHTML +=
+            '<span class="star"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px"><path d="M12 .587l3.668 7.568L24 9.748l-6 5.85 1.416 8.269L12 18.896l-7.416 3.97L6 15.598l-6-5.85 8.332-1.593L12 .587z" fill="#dcdad9"/></svg></span>'; // Add half star if needed
+        }
+
+        return starsHTML;
+      }
+
+      // Function to update product rating
+      function updateProductRating(productId, rating) {
+        // Here you can implement logic to update the rating for the product with the given productId
+        // For example, if you have an array of products, you can find the product by ID and update its rating
+        const product = data.items.find((item) => item.id === productId);
+        if (product) {
+          product.rating = rating;
+        }
+      }
+
+      // Get the container element where items will be appended
+      var productList = document.querySelector(".product-list");
+
+      // Function to display items based on filter with animations
+      function displayItems(items) {
+        productList.innerHTML = "";
+        items.slice(0, 3).forEach(function (item) {
+          var itemElement = createItemElement(item);
+          productList.appendChild(itemElement);
+        });
+        void productList.offsetWidth;
+        productList.querySelectorAll(".item").forEach(function (item) {
+          item.classList.add("show");
+        });
+      }
+
+      // Function to display initial items when the page loads
+      function displayInitialItems() {
+        displayItems(data.items.slice(0, 3));
+      }
+
+      // Function to handle filtering of items
+      function handleFiltering(event) {
+        event.preventDefault();
+        var filterValue = this.getAttribute("data-filter");
+        var filteredItems = data.items.filter(function (item) {
+          return filterValue === "all" || item.type === filterValue;
+        });
+
+        var maxItems = window.innerWidth <= 413 ? 3 : 3;
+        productList.querySelectorAll(".item").forEach(function (item) {
+          item.classList.add("hide");
+        });
+
+        setTimeout(function () {
+          productList.innerHTML = "";
+          filteredItems.slice(0, maxItems).forEach(function (item) {
+            var itemElement = createItemElement(item);
+            productList.appendChild(itemElement);
+          });
+          void productList.offsetWidth;
+          productList.querySelectorAll(".item").forEach(function (item) {
+            item.classList.add("show");
+          });
+        }, 500);
+
+        // Remove 'active' class from all filter links and add it to the clicked link
+        filterLinks.forEach(function (link) {
+          link.classList.remove("active");
+        });
+        this.classList.add("active");
+      }
+
+      // Function to handle rating of items
+      function handleRating(event) {
+        event.preventDefault();
+        const rating = parseInt(this.getAttribute("data-value"));
+        const productId = parseInt(this.closest(".item").dataset.id);
+        updateProductRating(productId, rating);
+        const messageContainer =
+          this.closest(".item").querySelector(".rate-message");
+        messageContainer.textContent = `You rated this product ${rating} stars!`;
+      }
+
+      // Get all the filter links
+      var filterLinks = document.querySelectorAll(".filter-button");
+
+      // Event listener for clicking the filter icon to display initial items
+      var filterIcon = document.querySelector(".category img");
+      filterIcon.addEventListener("click", function () {
+        displayInitialItems();
+      });
+
+      // Display initial items when the page loads
+      displayInitialItems();
+
+      // Add click event listeners to the filter links
+      filterLinks.forEach(function (link) {
+        link.addEventListener("click", handleFiltering);
+      });
+
+      // Add click event listeners to the rating stars
+      var stars = document.querySelectorAll(".star");
+      stars.forEach(function (star) {
+        star.addEventListener("click", handleRating);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching JSON data:", error);
+    });
+});
+
+// ---------------- End of Products Filtering and Fetching ---------------------
 
 // ---------------- Start of Professional Education Filtering and Fetching ---------------------
 /**
@@ -238,9 +437,4 @@ setInterval(updateTicker, 60000); // Update every minute
 updateTicker(); // Initial call
 
 // ----------------End of Footer Ticker ---------------------
-
-
-
-
-
 
